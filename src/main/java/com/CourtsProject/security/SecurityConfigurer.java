6ab722374d55ded20813359e6be0049461700de0
Cezary Muszyński <cezary.m.muszynski@gmail.com>
2020-08-21 02:00:48 +0200

    Changes in pom.xml. Dependencies seems to be proper now.

---
+++ src/main/java/com/CourtsProject/security/SecurityConfigurer.java
@@ -0,0 +1,6 @@
+package com.CourtsProject.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+}
